3ce9dc8fe768ca759a02677394c470ce29d38679
MehreenZahra <codewithMehreen1@gmail.com>
2025-04-23 21:12:01 +0500

    feat: Sanity-Added user SchemaType

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,5 +1,7 @@
-import { type SchemaTypeDefinition } from 'sanity'
+import { type SchemaTypeDefinition } from "sanity";
+import { userType } from "./userType";
+import { postType } from "./postType";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [],
-}
+  types: [userType],
+};
